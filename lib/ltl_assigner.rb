d9767ba53e6ece928c20dc4fe50e27c8f5b0c8df
brettwgreen <bw_green@yahoo.com>
2019-12-20 09:41:55 -0500

    adds tests hitting the actual class
    master

--- lib/ltl_assigner.rb
+++ lib/ltl_assigner.rb
@@ -1,9 +1,16 @@
 class LtlAssigner
-  def init(trucks, shipments)
+  attr_accessor :trucks, :shipments
+
+  def initialize(trucks, shipments)
+    @trucks = trucks
+    @shipments = shipments
   end
 
   def calculate_distributions
     return []
   end
 
+  def total_shipping_capacity
+    trucks.map{|t| t["capacity"]}.reduce(:+)
+  end
 end
--- spec/lib/ltl_assigner_spec.rb
+++ spec/lib/ltl_assigner_spec.rb
@@ -4,10 +4,19 @@ require 'yaml'
 require File.expand_path('../../../lib/ltl_assigner', __FILE__)
 
 RSpec.describe LtlAssigner, "#calculate_distributions" do
+  before(:each) do
+    @trucks = YAML.load_file("trucks.yaml").collect{|_k, v| v}
+    @shipments = YAML.load_file("shipments.yaml").collect{|_k, v| v}
+    @ltl_assigner = LtlAssigner.new(@trucks, @shipments)
+  end
+
   context "given sample trucks and shipments" do
     it "returns evenly distributed shipments" do
-      trucks = YAML.load_file("trucks.yaml")
-      expect(trucks.length).to eq 4
+      expect(@trucks.length).to eq 4
+    end
+
+    it "can get total shipping capcity" do
+      expect(@ltl_assigner.total_shipping_capacity).to eq 130000
     end
   end
 end
